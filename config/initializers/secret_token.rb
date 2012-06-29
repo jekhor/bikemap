830afdd7b3f6e07b5539b608043216846f182b19
Yauhen Kharuzhy <jekhor@gmail.com>
2012-06-29 16:51:36 +0300

    Initial commit
    Signed-off-by: Yauhen Kharuzhy <jekhor@gmail.com>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MapEverything::Application.config.secret_token = '7c9fd9cde005c6d13a89abfcc747df702250ef55aa84098263649ab0a8afd275cbe7d2c46392d8388554c4a5eec3c0b33217c8131d905ce8c8f396071c1c90fe'
