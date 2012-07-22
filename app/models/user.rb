e8ba4e6423b19ff186a58dc490af345aab44298e
Yauhen Kharuzhy <jekhor@gmail.com>
2012-07-22 22:51:51 +0300

    Add ability to login with name OR email
    Signed-off-by: Yauhen Kharuzhy <jekhor@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,9 @@ class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation, :remember_me
   attr_accessible :provider, :uid
 
+  attr_accessor :login
+  attr_accessible :login
+
   def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
     user = User.where(:provider => auth.provider, :uid => auth.uid).first
     unless user
@@ -30,6 +33,15 @@ class User < ActiveRecord::Base
       if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
         user.email = data["email"] if user.email.blank?
       end
+    end
+  end
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["lower(name) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      where(conditions).first
     end
   end
 
