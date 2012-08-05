c5e6a93499c3a7db92d51298272716d7ef7c7271
Yauhen Kharuzhy <jekhor@gmail.com>
2012-08-05 22:51:58 +0300

    Remove unneeded create method from users controller
    Signed-off-by: Yauhen Kharuzhy <jekhor@gmail.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -39,22 +39,6 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
-  # POST /users
-  # POST /users.json
-  def create
-    @user = User.new(params[:user])
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render json: @user, status: :created, location: @user }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
   # PUT /users/1
   # PUT /users/1.json
   def update
