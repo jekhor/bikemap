3af0fd7e6b5de553a50c52f97eab109ef2b6766a
Yauhen Kharuzhy <jekhor@gmail.com>
2012-08-16 16:43:22 +0300

    Remove notices about success authentication
    Signed-off-by: Yauhen Kharuzhy <jekhor@gmail.com>

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -6,7 +6,7 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.find_for_facebook_oauth(request.env["omniauth.auth"], current_user)
 
     if @user.persisted?
-      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Facebook"
+#      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Facebook"
       sign_in_and_redirect @user, :event => :authentication
     else
       session["devise.facebook_data"] = request.env["omniauth.auth"]
@@ -20,7 +20,7 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.find_for_vkontakte_oauth(request.env["omniauth.auth"], current_user)
 
     if @user.persisted?
-      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "ВКонтакте"
+#      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "ВКонтакте"
       sign_in_and_redirect @user, :event => :authentication
     else
       session["devise.vkontakte_data"] = request.env["omniauth.auth"]
@@ -34,7 +34,7 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.find_for_google_oauth2(request.env["omniauth.auth"], current_user)
 
     if @user.persisted?
-      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
+#      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
       sign_in_and_redirect @user, :event => :authentication
     else
       session["devise.google_oauth2_data"] = request.env["omniauth.auth"]
@@ -48,7 +48,7 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.find_for_mailru_oauth(request.env["omniauth.auth"], current_user)
 
     if @user.persisted?
-      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Mail.Ru"
+#      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Mail.Ru"
       sign_in_and_redirect @user, :event => :authentication
     else
       session["devise.mailru_oauth_data"] = request.env["omniauth.auth"]
